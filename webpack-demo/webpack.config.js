a401867950af17efe450ded4e6572e282f666fae
黄卉 <326028030@qq.com>
2017-03-09 10:40:43 +0800

    修改

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,14 +1,17 @@
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './entry.js',
-  output: {
+    entry: './entry.js',
+    output: {
     path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {test: /\.css$/, lader: 'style!css'}
-    ]
-  }
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+          {test: /\.css$/, loader: 'style-loader!css-loader'}  //20170309，-loader一定不能省略
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin('作者：黄卉')
+    ] 
 }
